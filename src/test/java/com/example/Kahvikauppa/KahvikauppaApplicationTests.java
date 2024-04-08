37092da5c989c457f47ef69e752c9d36347cae31
Alexei <>
2024-04-08 13:05:50 +0300

    rakenne

---
+++ src/test/java/com/example/Kahvikauppa/KahvikauppaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Kahvikauppa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KahvikauppaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
